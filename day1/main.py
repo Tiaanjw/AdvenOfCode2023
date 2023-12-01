e1ea545d4e07bbba0bd28908f418fffa69c45f61
tiaanjw <tiaanjw@gmail.com>
2023-12-01 15:09:04 +0100

    fix: clean up unused code

--- day1/main.py
+++ day1/main.py
@@ -30,7 +30,6 @@ def findMatches(inputString, dict):
       i += 1
   return matches
 
-completeList = []
 digitDictionary = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"}
 for line in readFile(puzzleInputFile).splitlines():
   res = findMatches(line, digitDictionary)
