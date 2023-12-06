960cfcc7010cea1f79fa860a23851287fa11a4d0
tiaanjw <tiaanjw@gmail.com>
2023-12-06 10:26:58 +0100

    final code cleanup

--- day5/main.py
+++ day5/main.py
@@ -1,109 +1,74 @@
 import re
-import threading
 import time
 import multiprocessing
 
-seeds = []
-seedsRanges = []
-maps = {}
-phase = 0
-
-puzzleInputFile = "puzzleInput.txt"
-
 def readFile(fileName):
   with open(fileName, "r") as file:
-    return file.read().splitlines()
+    return file.read()
   
-puzzleInput = readFile(puzzleInputFile)
+def parsePuzzleInput(puzzleInputFile):
+  seeds = []
+  seedsRanges = []
+  maps = {}
+  phase = 0
 
-for idx, line in enumerate(puzzleInput):
-  if idx == 0:
-    seeds = [int(x) for x in line.split(': ')[1].split(' ')]
-    for idx, seed in enumerate(seeds):
-      if idx % 2:
-        pass
-      else:
-        seedsRanges.append([seed, seeds[idx+1]])
-    continue
+  for idx, line in enumerate(readFile(puzzleInputFile).splitlines()):
+    if idx == 0:
+      seeds = [int(x) for x in line.split(': ')[1].split(' ')]
+      for idx, seed in enumerate(seeds):
+        if idx % 2:
+          pass
+        else:
+          seedsRanges.append([seed, seeds[idx+1]])
+      continue
 
-  if line == '':
-    continue
+    if line == '':
+      continue
+    
+    if re.search('[a-zA-Z]', line):
+      phase += 1
+      maps[phase] = []
+      continue
+
+    phaseInfo = line.split(' ')
+
+    source = int(phaseInfo[1])
+    destination = int(phaseInfo[0])
+    rangeInfo = int(phaseInfo[2])-1
+
+    maps[phase].append([[source, source+rangeInfo], [destination, destination+rangeInfo]])
+
+  return {"maps": maps, "seeds": seeds, "seedsRanges": seedsRanges}
+
+def partOne(puzzleInput):
+  seeds = puzzleInput["seeds"]
+  maps = puzzleInput["maps"]
+  phaseValues = {}
+  lowestValue = 0
+
+  for seed in seeds:
+    phaseValues[seed] = seed
+    for phase, values in maps.items():
+      for phaseRanges in values:
+        if phaseRanges[0][0] <= phaseValues[seed] <= phaseRanges[0][1]:
+          index = phaseValues[seed] - phaseRanges[0][0]
+          phaseValues[seed] = phaseRanges[1][0] + index
+          break
   
-  if re.search('[a-zA-Z]', line):
-    phase += 1
-    maps[phase] = []
-    continue
+  for seed, value in phaseValues.items():
+    if lowestValue == 0 or value < lowestValue:
+      lowestValue = value
 
-  phaseInfo = line.split(' ')
-
-  source = int(phaseInfo[1])
-  destination = int(phaseInfo[0])
-  rangeInfo = int(phaseInfo[2])-1
-
-  maps[phase].append([[source, source+rangeInfo], [destination, destination+rangeInfo]])
-
-
-
-# # print(puzzleInput)
-# seeds = []
-# maps = {}
-# phase = 0
-
-# for idx, line in enumerate(puzzleInput):
-#   if idx == 0:
-#     seeds = [int(x) for x in line.split(': ')[1].split(' ')]
-#     # print("seeds: " + str(seeds))
-#     continue
-
-#   if line == '':
-#     continue
-  
-#   if re.search('[a-zA-Z]', line):
-#     phase += 1
-#     maps[phase] = []
-#     continue
-
-#   phaseInfo = line.split(' ')
-#   # print("phase: " + str(phase) + ". ", phaseInfo)
-
-#   source = int(phaseInfo[1])
-#   destination = int(phaseInfo[0])
-#   rangeInfo = int(phaseInfo[2])-1
-
-#   maps[phase].append([[source, source+rangeInfo], [destination, destination+rangeInfo]])
-
-# phaseValues = {}
-# for seed in seeds:
-#   phaseValues[seed] = seed
-#   for phase, values in maps.items():
-#     for phaseRanges in values:
-      
-#       if phaseRanges[0][0] <= phaseValues[seed] <= phaseRanges[0][1]:
-#         index = phaseValues[seed] - phaseRanges[0][0]
-#         phaseValues[seed] = phaseRanges[1][0] + index
-#         break
-
-# lowestValue = 0
-# for seed, value in phaseValues.items():
-#   if lowestValue == 0 or value < lowestValue:
-#     lowestValue = value
-
-# print("part 1: " + str(lowestValue))
-
+  return lowestValue
 
 
 #----- PART 2
 
-
-
-
-def get_lowest_from_range(seedRange, lvs):
-  # print(maps)
-  # print(seedRange)
+def get_lowest_from_range(seedRange, lvs, maps):
+  phaseValues = {}
   seed = seedRange[0]
   lowestValue = 0
   while seed <= seedRange[1]:
-    # print("seed: " + str(seed))
     phaseValues[seed] = seed
     for phase, values in maps.items():
       for phaseRanges in values:
@@ -114,55 +79,43 @@ def get_lowest_from_range(seedRange, lvs):
     if lowestValue == 0 or phaseValues[seed] < lowestValue:
       lowestValue = phaseValues[seed]
     seed += 1
-  # print("lowest: " + str(lowestValue))
+
   lvs.append(lowestValue)
 
 
-
-
-phaseValues = {}
-lowestValue = 0
-threads = {}
-tidx = 0
-
-if __name__ == "__main__":
+def partTwo(puzzleInput):
+  maps = puzzleInput["maps"]
+  seedsRanges = puzzleInput["seedsRanges"]
+  procs = {}
   manager = multiprocessing.Manager()
   lowestValues = manager.list()
-  
-
 
   for seedRange in seedsRanges:
-    # print(seedRange)
     start = seedRange[0]
     end = seedRange[0] + seedRange[1] - 1 
     batch = 1000000
     idx = 0
-    # print([start, end], start+batch)
-    while start < end: # 99 < 92
+
+    while start < end:
       idx += 1
-      nextRange = start + batch # 89 + 10 = 99
-      
-      if start + batch > end: # 89 + 10 = 99 > 92
-        nextRange = end # 92
-      rangeToCheck = [start, nextRange] # 89,92
-      # print("range to check: " + str(rangeToCheck))
-      p = multiprocessing.Process(target=get_lowest_from_range, args=[rangeToCheck, lowestValues])
-      threads[idx] = p
+      nextRange = start + batch
+      if start + batch > end:
+        nextRange = end
+      rangeToCheck = [start, nextRange]
+
+      p = multiprocessing.Process(target=get_lowest_from_range, args=[rangeToCheck, lowestValues, maps])
+      procs[idx] = p
       p.start()
-      # print("starting thread: " + str(idx))
-      start += batch # 99
-      # break
-    # break
+      start += batch
 
-    # print(threads)
-    for idx, proc in threads.items():
+    for idx, proc in procs.items():
       while proc.is_alive():
-        print("threads: " + str(len(threads)))
-        time.sleep(1)
+        # print("Procs: " + str(len(procs)))
+        time.sleep(0.5)
 
+  return min(lowestValues)
 
-
-  # print(lowestValues)
-  lowestValue = min(lowestValues)
-
-  print("part 2: " + str(lowestValue))
+if __name__ == "__main__":
+  puzzleInput = parsePuzzleInput("puzzleInput.txt")
+  print("Part 1: " + str(partOne(puzzleInput)))
+  print("Part 2: " + str(partTwo(puzzleInput)))
